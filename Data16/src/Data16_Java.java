2f0b6fbbff0c93d5cc61d49b753296926bc8f955
shfkf <shfkf@DESKTOP-DJHLVC2>
2020-07-26 17:34:02 +0900

    학교에서 자바 학습

---
+++ Data16/src/Data16_Java.java
@@ -0,0 +1,62 @@
+//import java.util.Scanner;
+public class Data16_Java {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		Scanner scn = new Scanner(System.in);
+		float result;
+		float a;
+		float b;
+		
+	
+		System.out.print("첫 번째 숫자를 입력하세요:");
+		a = scn.nextFloat();
+		
+		System.out.print("두 번째 숫자를 입력하세요:");
+		b = scn.nextFloat();
+
+		result = a + b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a / b;
+		System.out.print(a + "/" + b + "=" + (int)result);
+		*/
+		
+		int intValue = 44032;
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+
+		long longValue = 500;
+		intValue = (int) longValue;
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int) doubleValue;
+		System.out.println(intValue);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
